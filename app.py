85cd7dc0107e5415d161d28ac6ab1ed4a14b9153
Andreas Gropp <starmaerker@googlemail.com>
2020-03-24 09:26:44 +0100

    basic setup

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask
+from flask import render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/katalog')
+def katalog():
+    return 'katalog'
+
+@app.route('/rangliste')
+def rangliste():
+    return 'rangliste'
 
 
 if __name__ == '__main__':
